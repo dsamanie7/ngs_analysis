259d561287f94588034db621e2b96dc2c17a9d0d
dsamanie7 <danielasc92@gmail.com>
2024-12-13 02:05:13 -0600

    added star quantMode, samtools sort + index, and feature counts

--- rna_seq/run_star_align_n_count.sh
+++ rna_seq/run_star_align_n_count.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-run_star_alignment() {
+run_star_align_n_count() {
     # Function to generate a SLURM job for STAR alignment for a single sample
     # Arguments:
     #   --sample_name: Sample name (e.g., "Sample1")
@@ -10,6 +10,7 @@ run_star_alignment() {
 
     # Default values for directories
     local GENOME_DIR="/projects/p31767/Bioinformatics/mouse/star/star_50/"
+    local GTF_FILE="/projects/p31767/Bioinformatics/mouse/mm10/annotation_gencode_vM23/gencode.vM23.annotation.gtf"
     local INPUT_DIR="/projects/b1042/Shukla_lab/Daniela/projects/ngs_default_analysis/input_files"
     local OUTPUT_DIR="/projects/b1042/Shukla_lab/Daniela/projects/ngs_default_analysis/output_analysis"
     local SAMPLE_NAME=""
@@ -29,6 +30,10 @@ run_star_alignment() {
                 ;;
             --genome_dir)
                 GENOME_DIR="$2"
+                shift 2
+                ;;
+            --gtf_file)
+                GTF_FILE="$2"
                 shift 2
                 ;;
             --output_dir)
@@ -51,15 +56,16 @@ run_star_alignment() {
     done
 
     if [ -z "$SAMPLE_NAME" ]; then
-        echo -e "\nUsage: run_star_alignment --sample_name <SAMPLE_NAME> [--input_dir <INPUT_DIR>] [--genome_dir <GENOME_DIR>] [--output_dir <OUTPUT_DIR>] [--single_end] [--submit]\n"
+        echo -e "\nUsage: run_star_align_n_count.sh --sample_name <SAMPLE_NAME> [--input_dir <INPUT_DIR>] [--genome_dir <GENOME_DIR>] [--genome_ann <GENOME_ANN_DIR>] [--output_dir <OUTPUT_DIR>] [--single_end] [--submit]\n"
         echo "Requirements:"
         echo "  - STAR aligner installed and accessible from PATH"
         echo "  - Input files located in the specified input directory or default: $INPUT_DIR"
         echo "  - Genome directory specified or default: $GENOME_DIR"
+        echo "  - GTF annotation file specified or default: $GTF_FILE"
         echo "  - Output directory specified or default: $OUTPUT_DIR"
         echo "  - Specify single-end with '--single_end'. Default assumes Paired-End data"
         echo "  - Use '--submit' to submit the SLURM job after creation"
-        printf "\nExample: run_star_alignment.sh --sample_name Sample1 --submit\n\n"
+        printf "\nExample: run_star_align_n_count.sh --sample_name Sample1 --submit\n\n"
         return 1
     fi
 
@@ -78,7 +84,7 @@ run_star_alignment() {
     local TIMESTAMP=$(date +%Y%m%d)
 
     # SLURM script content
-    local SLURM_SCRIPT="$OUTPUT_DIR/slurm_logs/star_mapping_${TIMESTAMP}_${SAMPLE_NAME}.slurm"
+    local SLURM_SCRIPT="$OUTPUT_DIR/slurm_logs/star_map_n_count_${TIMESTAMP}_${SAMPLE_NAME}.slurm"
     cat <<EOT > "$SLURM_SCRIPT"
 #!/bin/bash
 #SBATCH --account=b1042
@@ -87,9 +93,9 @@ run_star_alignment() {
 #SBATCH --ntasks=20
 #SBATCH --mem=64G
 #SBATCH --time=24:00:00
-#SBATCH --job-name=star_mapping_${SAMPLE_NAME}
-#SBATCH --output=${OUTPUT_DIR}/slurm_logs/star_mapping_${TIMESTAMP}_${SAMPLE_NAME}.out
-#SBATCH --error=${OUTPUT_DIR}/slurm_logs/star_mapping_${TIMESTAMP}_${SAMPLE_NAME}.err
+#SBATCH --job-name=star_map_n_count_${SAMPLE_NAME}
+#SBATCH --output=${OUTPUT_DIR}/slurm_logs/star_map_n_count_${TIMESTAMP}_${SAMPLE_NAME}.out
+#SBATCH --error=${OUTPUT_DIR}/slurm_logs/star_map_n_count_${TIMESTAMP}_${SAMPLE_NAME}.err
 
 STAR --runThreadN 16 \
      --genomeDir "$GENOME_DIR" \
@@ -104,7 +110,27 @@ STAR --runThreadN 16 \
      --alignIntronMax 1000000 \
      --alignMatesGapMax 100000 \
      --genomeFileSizes "$GENOME_DIR/chrLength.txt" \
+     --quantMode GeneCounts \
      --outFileNamePrefix "$OUTPUT_DIR/${SAMPLE_NAME}-"
+
+# Binarize, sort and index alignment
+module load samtools
+samtools view -bS $OUTPUT_DIR/${SAMPLE_NAME}-Aligned.out.sam > $OUTPUT_DIR/${SAMPLE_NAME}-Aligned.out.bam
+samtools sort -@ 16 -o $OUTPUT_DIR/${SAMPLE_NAME}-Aligned_sorted.out.bam $OUTPUT_DIR/${SAMPLE_NAME}-Aligned.out.bam
+samtools index $OUTPUT_DIR/${SAMPLE_NAME}-Aligned_sorted.out.bam
+
+rm $OUTPUT_DIR/${SAMPLE_NAME}-Aligned.out.sam
+rm $OUTPUT_DIR/${SAMPLE_NAME}-Aligned.out.bam
+
+# Get count data Feature Counts
+module load subreads
+
+featureCounts -p -B -g gene_id \
+  -s 2 -S rf \
+  -a ${GTF_FILE} \
+  -o $OUTPUT_DIR/feature_counts_${SAMPLE_NAME}_s2_rf.txt \
+  $OUTPUT_DIR/${SAMPLE_NAME}-Aligned_sorted.out.bam
+
 EOT
 
     echo "SLURM job script generated: $SLURM_SCRIPT"
@@ -115,5 +141,5 @@ EOT
 }
 
 if [[ "$0" == "$BASH_SOURCE" ]]; then
-    run_star_alignment "$@"
+    run_star_align_n_count "$@"
 fi
